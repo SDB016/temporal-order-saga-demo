a86d59cf3e6d6ed47ebeea39d8d60f19ec73e6a4
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2024-12-02 23:33:06 +0900

    payment 모듈 temporal 설정 추가

--- payment/src/main/kotlin/com/demo/temporal/payment/activity/PaymentActivitiesImpl.kt
+++ payment/src/main/kotlin/com/demo/temporal/payment/activity/PaymentActivitiesImpl.kt
@@ -3,15 +3,15 @@ package com.demo.temporal.payment.activity
 import io.github.oshai.kotlinlogging.KotlinLogging
 
 class PaymentActivitiesImpl : PaymentActivities {
-
     private val logger = KotlinLogging.logger { }
+
     override fun processPayment(orderId: String) {
-        logger.info("Processing payment for order: $orderId")
+        logger.info { "Processing payment for order: $orderId" }
 
     }
 
     override fun refundPayment(orderId: String) {
-        logger.info("Refunding payment for order: $orderId")
+        logger.info { "Refunding payment for order: $orderId" }
 
     }
 
